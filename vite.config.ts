471b3adcdf7b4a94f004380aaf89a3d9175650dd
engalar <engalar@gmail.com>
2024-05-07 20:44:03 +0800

    (feat): add dynamic widget name

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Inspect from "vite-plugin-inspect";
 import mendix from "@engalar/vite-plugin-mendix";
+// @ts-ignore
+import pkg from "./package.json";
 
 export default defineConfig({
-    plugins: [Inspect(), react(), mendix({ widgetName: "AddressFinder" })]
+    plugins: [Inspect(), react(), mendix({ widgetName: pkg.widgetName })]
 });
